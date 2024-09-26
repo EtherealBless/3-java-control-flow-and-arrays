e225e3491aa768432a120454746f2d7ec546da42
EtherealBless <connection.has.fallen@gmail.com>
2024-09-27 01:16:09 +0500

    Done

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -10,8 +10,14 @@ public class Task06Main {
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        return a > b ?
+                (a > c ?
+                        (a > d ? a : d) :
+                        (c > d ? c : d)) :
+                (b > c ?
+                        (b > d ? b : d) :
+                        (c > d ? c : d));
+
     }
 
 }
